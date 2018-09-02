7f0a3ab0f4a94e0421162353e46efa1d13f4b65c
L <leichen070@gmail.com>
2018-09-02 19:50:37 +0800

    能正常训练和使用

--- verificationCode_1/gen_captcha.py
+++ verificationCode_1/gen_captcha.py
@@ -10,9 +10,15 @@ import os
 number = ['0','1','2','3','4','5','6','7','8','9']
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','    v','w','x','y','z']
 ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','    V','W','X','Y','Z']
+
+# 图像大小
+IMAGE_HEIGHT = 60  #80
+IMAGE_WIDTH = 160  #250
+MAX_CAPTCHA = 4
+trainImagePath='./trainImage'
  
 # 验证码长度为4个字符
-def random_captcha_text(char_set=number, captcha_size=4):
+def random_captcha_text(char_set=number, captcha_size=MAX_CAPTCHA):
     captcha_text = []
     for i in range(captcha_size):
         c = random.choice(char_set)
@@ -20,7 +26,7 @@ def random_captcha_text(char_set=number, captcha_size=4):
     return captcha_text
  
 # 生成字符对应的验证码
-def gen_captcha_text_and_image():
+def gen_captcha_text_and_image_r():
     image = ImageCaptcha()
     captcha_text = random_captcha_text(char_set=number)
     captcha_text = ''.join(captcha_text)    #用空字符串来连接4个字符集合
@@ -29,16 +35,24 @@ def gen_captcha_text_and_image():
     captcha_image = Image.open(captcha)     #获取图片信息
     captcha_image = np.array(captcha_image) #将图片转行成np数组
     return captcha_text, captcha_image
+
+def func1():
+    for i in range(10):
+        text, image = gen_captcha_text_and_image_r()
+        fullPath = os.path.join(trainImagePath, text + ".jpg")
+	print fullPath
+        #cv2.imwrite(fullPath, image, [int(cv2.IMWRITE_JPEG_QUALITY),9])
+        image=cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT) )
+        cv2.imwrite(fullPath, image )
+        #print "{0}/10000".format(i)
+    print "/nDone!"
  
 if __name__ == '__main__':
-    #保存路径
-    path = './trainImage'
-    # path = './validImage'
-    for i in range(3000):
-        text, image = gen_captcha_text_and_image()
-        fullPath = os.path.join(path, text + ".jpg")
-        cv2.imwrite(fullPath, image)
-        print "{0}/10000".format(i)
-    print "/nDone!"
-
-
+	func1()
+	'''
+    image=ImageCaptcha()
+    captcha = image.generate('1234')
+    captcha_image = Image.open(captcha)
+    captcha_image = np.array(captcha_image)
+    print captcha_image
+	'''
