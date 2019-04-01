37d01e5d7ba06154e31a7d4a7ba59dd265af134d
Lmbp <leichen070@gmail.com>
2019-04-01 13:51:01 +0800

    回顾

--- basisLearn/3-/2-session.py
+++ basisLearn/3-/2-session.py
@@ -7,18 +7,15 @@ import tensorflow as tf
 '''
 1. session会话控制 Session 是 Tensorflow 为了控制,和输出文件的执行的语句. 运行 session.run() 可以获得你要得知的运算结果, 或者是你所要运算的部分.
 '''
-m1 = tf.constant([[3,3]])   #constant常数
-m2 = tf.constant([[2],
-                 [2]])
+m1 = tf.constant([[3,3]])   #constant常数,一个二维的一行两列(1,2)的张量
+m2 = tf.constant([[2],[2]]) #constant常数，一个二维的2行1列(2,1)的张量
 product = tf.matmul(m1, m2) #m1*m2 此时并没有正在的开始计算
 
-'''
 #方法1
 sess = tf.Session()
 result = sess.run(product)
 print(result)
 sess.close()
-'''
 
 '''
 #方法2
@@ -32,7 +29,7 @@ print(' ----------------------------------------------------------------------')
 2.Variable 变量
 '''
 state = tf.Variable(0, name='counter')  #定义一个变量
-one = tf.constant(1) #定义一个常量
+one = tf.constant(1) #定义一个常量,
 new_value = tf.add(state, one) #定义加法步骤 (注: 此步并没有直接计算)
 update = tf.assign(state, new_value) #将 State 更新成 new_value
 #初始化
@@ -53,8 +50,8 @@ placeholder 是 Tensorflow 中的占位符，暂时储存变量.
 i1 = tf.placeholder(tf.float32) #定义两个占位符
 i2 = tf.placeholder(tf.float32)
 
-o = tf.multiply(i1, i2) #普通乘法
-#o = tf.matmul(i1,i2) #矩阵乘法
+#o = tf.multiply(i1, i2) #普通乘法
+o = tf.matmul(i1,i2) #矩阵乘法
 
 with tf.Session() as sess:
     print(sess.run(o, feed_dict={i1:[[7,2]],i2:[[2],[3]]}))
---
+++ basisLearn/3-/4-.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+'''
+加速神经网络训练
+有几种模式：
+    Stochastic Gradient Descent (SGD) #随机梯度下降，更新神经网络参数时加速
+    Momentum    #动量,相当于给个斜坡
+    AdaGrad #相当于阻止走弯路
+    RMSProp #以上两种结合
+    Adam    #以上两种更好的结合
+'''
