75b3efd984de06f3c0112eba58c603cae6df9717
Lmbp <leichen070@gmail.com>
2019-05-20 17:25:42 +0800

    回顾变量写法

--- basisLearn/3-/1-placeholder.py
+++ basisLearn/3-/1-placeholder.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 '''
 基础学习：https://morvanzhou.github.io/tutorials/machine-learning/tensorflow/2-2-example2/
-可以和1-mnist/2-test.py或者1-placeholder.py结合相看，两种写法
+可以和1-.py结合相看，两种写法
 '''
 import tensorflow as tf
 import numpy as np
@@ -11,12 +11,15 @@ x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data*0.1+0.3
 
 #搭建模型
+x=tf.placeholder(tf.float32, [None])
+y=tf.placeholder(tf.float32, [None])
 weights=tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases = tf.Variable(tf.zeros([1]))
-y=weights*x_data+biases
+y_model=weights*x+biases
+
 
 #计算误差
-loss = tf.reduce_mean(tf.square(y-y_data))
+loss = tf.reduce_mean(tf.square(y-y_model))
 
 #用梯度下降法传播误差
 optimizer = tf.train.GradientDescentOptimizer(0.5)
@@ -28,6 +31,9 @@ sess = tf.Session()
 sess.run(init)
 
 for step in range(201):
-    sess.run(train)
+    sess.run(train,feed_dict={x:x_data, y:y_data})
     if step % 20 == 0:
-        print(step, sess.run(weights), sess.run(biases), sess.run(loss))
+        w_run=sess.run(weights)
+        b_run=sess.run(biases)
+        loss_run=sess.run(loss, feed_dict={x:x_data, y:y_data})
+        print(step,w_run,b_run,loss_run)
